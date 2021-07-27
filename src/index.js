7fadc2a9074bdcc274734299a1bac91251699a93
sarthak78381 <sarthakg2k29@gmail.com>
2021-07-28 00:23:13 +0530

    corrected path to locate client folder

--- src/index.js
+++ src/index.js
@@ -16,14 +16,11 @@ app.use(cookieParser())
 
 const port = process.env.PORT;
 
-
-
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../', 'client/build')));
   
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../', 'client/build', 'index.html'));
   });
 }
 
